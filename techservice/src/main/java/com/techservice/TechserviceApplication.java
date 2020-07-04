5eabc87bb95fdfb7a48da7f1d110db5ccaba4099
selvapandymoorthy <selvapandymoorthy@localhost>
2020-07-04 18:06:32 -0400

    first commit

---
+++ techservice/src/main/java/com/techservice/TechserviceApplication.java
@@ -0,0 +1,13 @@
+package com.techservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechserviceApplication.class, args);
+	}
+
+}
---
+++ techservice/src/test/java/com/techservice/TechserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
